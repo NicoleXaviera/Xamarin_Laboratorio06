c2b10c103591cab4c38c7774aaa9a42318e4c625
Alumno <Alumno@LAB1509-23>
2023-10-13 15:20:21 -0500

    Agregar archivos de proyecto.

---
+++ ListViewExample/ListViewExample/ListViewSource.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ListViewExample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewSource : ContentPage
+    {
+        ObservableCollection<Employee> employees = new ObservableCollection<Employee>();
+
+        public ObservableCollection<Employee> Employees { get { return employees; } }
+        public ListViewSource()
+        {
+            InitializeComponent();
+            //ObservableCollection allows to be added after ItemsSource
+            //is set and the UI will react to changes
+            EmployeeView.ItemsSource = employees;
+            employees.Add(new Employee { DisplayName = "Rob Finnerty" });
+            employees.Add(new Employee { DisplayName = "Xaviera Ugarriza" });
+            employees.Add(new Employee { DisplayName = "Gonzalo Lazo de la Vega" });
+            employees.Add(new Employee { DisplayName = "Jacko Tinoco" });
+            employees.Add(new Employee { DisplayName = "Fildel Matos" });
+            employees.Add(new Employee { DisplayName = "Pierina Barco" });
+        }
+    }
+}
